acec7149832c06a708abc246a8791a55f138056a
Michelin <michael.kazitsin@yandex.ru>
2019-03-28 21:29:06 +0300

    added moving

--- bigtask.py
+++ bigtask.py
@@ -6,6 +6,34 @@ import os
 spn = input()
 cord1, cord2 = input().split()
 
+
+def moove(arrow):
+    global spn, cord1, cord2
+    if arrow == 'left':
+        cord1 = float(cord1)
+        cord1 -= (float(spn) * 2)
+        cord1 = str(cord1)
+    elif arrow == 'right':
+        cord1 = float(cord1)
+        cord1 += (float(spn) * 2)
+        cord1 = str(cord1)
+    elif arrow == 'forward':
+        cord2 = float(cord2)
+        cord2 += (float(spn) * 2)
+        cord2 = str(cord2)
+    elif arrow == 'back':
+        cord2 = float(cord2)
+        cord2 -= (float(spn) * 2)
+        cord2 = str(cord2)
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={},{}&spn={},0.002&l=sat".format(cord1, cord2, spn)
+    response = requests.get(map_request)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    os.remove(map_file)
+
+
 def scaale(znak):
     global spn, cord1, cord2
     if znak == '+':
@@ -60,10 +88,25 @@ while running:
         counter = 1
         pygame.display.flip()
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == 276 and event.scancode == 75:
+            moove('left')
+            print(cord1, cord2)
+        if event.type == pygame.KEYDOWN and event.key == 275 and event.scancode == 77:
+            moove('right')
+            print(cord1, cord2)
+        if event.type == pygame.KEYDOWN and event.key == 273 and event.scancode == 72:
+            moove('forward')
+            print(cord1, cord2)
+        if event.type == pygame.KEYDOWN and event.key == 274 and event.scancode == 80:
+            moove('back')
+            print(cord1, cord2)
+
         if event.type == pygame.KEYDOWN and event.key == 280 and event.scancode == 73:
             scaale('+')
+            print(spn)
         if event.type == pygame.KEYDOWN and event.key == 281 and event.scancode == 81:
             scaale('-')
+            print(spn)
         if event.type == pygame.QUIT:
             running = False
 # Рисуем картинку, загружаемую из только что созданного файла.
